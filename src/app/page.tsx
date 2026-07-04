1fe8f08ab0e26d8b9f6f1b88861c6584fecbc760
KARTIK PANDEY <kartik0pandey00@gmail.com>
2026-07-04 15:00:06 +0530

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/sections/Hero";
 import StatsBar from "@/components/sections/StatsBar";
+import WelcomeSection from "@/components/sections/WelcomeSection";
 import ProductsGrid from "@/components/sections/ProductsGrid";
 import WhyChooseUs from "@/components/sections/WhyChooseUs";
 import SebiNotices from "@/components/sections/SebiNotices";
 import GrievanceQuickAccess from "@/components/sections/GrievanceQuickAccess";
 import PartnerCTA from "@/components/sections/PartnerCTA";
 import VoiceReader from "@/components/ui/VoiceReader";
+import FATCAAlert from "@/components/ui/FATCAAlert";
 
 export const metadata = {
   title: "Shri Venkatesh Stock Broker Services India Pvt. Ltd. | SEBI Registered Broker Raipur",
@@ -16,12 +18,14 @@ export default function Home() {
     <>
       <Hero />
       <StatsBar />
+      <WelcomeSection />
       <ProductsGrid />
       <WhyChooseUs />
       <SebiNotices />
       <GrievanceQuickAccess />
       <PartnerCTA />
       <VoiceReader />
+      <FATCAAlert />
     </>
   );
 }
